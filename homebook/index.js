d4552668f6f37e4a3e8fb34bddafea5e56fbe053
Stephen Poth <sspoth@gmail.com>
2019-03-07 00:43:18 -0800

    app navigation work
    laying down the bases for app navigation using react-native navigation

--- homebook/index.js
+++ homebook/index.js
@@ -1,10 +1,20 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
 
-import {AppRegistry} from 'react-native';
+
+import { Navigation } from "react-native-navigation";
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+
+
+
+Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setRoot({
+        root: {
+            component:{
+                name: "navigation.playground.WelcomeScreen"
+            }
+        }
+    });
+});
