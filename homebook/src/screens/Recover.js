0358eccd3eb451347cce315e705316fc0a42de21
Jake Vargas <jkvrgs85@gmail.com>
2019-04-04 20:49:39 -0700

    Update Recover.js

--- homebook/src/screens/Recover.js
+++ homebook/src/screens/Recover.js
@@ -34,8 +34,6 @@ class Recover extends Component {
         this.pushLoginScreen();
         }).catch(function (error) {
             alert(error);
-            // An error happened.
-            alert("Not a valid email address");
         });
 
 
