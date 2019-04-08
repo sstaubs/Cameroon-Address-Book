16af2775775424c33e14d567a98eb62e0e602570
Stephen Poth <sspoth@gmail.com>
2019-04-08 12:40:00 -0700

    confirm password

--- homebook/src/screens/CreateAccount.js
+++ homebook/src/screens/CreateAccount.js
@@ -10,7 +10,16 @@ class CreateAccount extends Component {
     lastName: '',
     email: '',
     password: '',
+    confirmpassword: '',
     phone: ''
+  };
+
+  passwordConfirm = () => {
+    if (this.state.password != this.state.confirmpassword) {
+      alert("Password does not match");
+      return false;
+    }
+    return true;
   };
 
   pushSetLocation = () => Navigation.push(this.props.componentId, {
@@ -55,43 +64,51 @@ class CreateAccount extends Component {
     });
   };
 
+  confirmPassHandler = val => {
+    this.setState({
+      confirmpassword: val,
+    });
+  };
+
   confirmHandler = val => {
+    if (this.passwordConfirm()) {
 
-    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(() => {
-        var user = firebase.auth().currentUser;
-        if (user) {
-          const accountInfo = {
-            firstN: this.state.firstName,
-            lastN: this.state.lastName,
-            email: this.state.email,
-            phoneNum: this.state.phone,
-            uid: user.uid
-          };
-          var db = firebase.firestore();
-          db.collection("users").add(accountInfo)
-            .then(function (docRef) {
-              //alert("Document written with ID: " + docRef.id);
-            })
-            .catch(function (error) {
-              console.error("Error adding document: ", error);
-            });
+      firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(() => {
+          var user = firebase.auth().currentUser;
+          if (user) {
+            const accountInfo = {
+              firstN: this.state.firstName,
+              lastN: this.state.lastName,
+              email: this.state.email,
+              phoneNum: this.state.phone,
+              uid: user.uid
+            };
+            var db = firebase.firestore();
+            db.collection("users").add(accountInfo)
+              .then(function (docRef) {
+                //alert("Document written with ID: " + docRef.id);
+              })
+              .catch(function (error) {
+                console.error("Error adding document: ", error);
+              });
 
-          this.pushSetLocation()
-        }
+            this.pushSetLocation()
+          }
 
-      })
-      .catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        if (errorCode == 'auth/weak-password') {
-          alert('The password is too weak.');
-        } else {
-          alert(errorMessage);
-        }
-        console.log(error);
-      });
+        })
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+        });
+    }
 
 
   };
@@ -134,7 +151,7 @@ class CreateAccount extends Component {
           style={styles.phoneInfo}
           placeholder='Enter Password'
           placeholderTextColor='gray'
-          onChangeText={this.passwordHandler}
+          onChangeText={this.confirmPassHandler}
         />
         <Text style={styles.sub2Text}>Phone Number</Text>
         <TextInput
